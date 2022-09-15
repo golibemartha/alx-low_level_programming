f414ceb524ff904c5fcc782c75b92ff21da1f58c
golibemartha <golibemartha@gmail.com>
2022-09-15 07:20:53 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - draw a stright line
+ * @n: number of times '_' is printed
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int s;
+
+	s = 0;
+	while (s < n)
+	{
+		_putchar('_');
+		c++;
+	}
+	_putchar('\n');
+}
