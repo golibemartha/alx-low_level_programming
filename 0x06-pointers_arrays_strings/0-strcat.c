8508a29f53874be78ab3f78d035c3193497f3c77
golibemartha <golibemartha@gmail.com>
2022-09-21 06:14:03 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+  * _strcat - function to concantenate two strings
+  * @dest: first char
+  * @src: second char
+  * Return: string dest
+  */
+char *_strcat(char *dest, char *src)
+{
+	int i, c;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+
+	c = 0;
+
+	while (src[c] != '\0')
+	{
+		dest[i] = src[c];
+		c++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
