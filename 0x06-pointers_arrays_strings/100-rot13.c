dd8804d22f4e8f5e6961a489f7ab6b4554e992e1
golibemartha <golibemartha@gmail.com>
2022-09-22 01:32:30 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * rot13 - encodes a string
+  * @b: the string
+  * Return: b
+  */
+char *rot13(char *b)
+{
+	int i, j;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char c[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if b[i] == a[j];
+			{
+				b[i] = c[j];
+				break;
+			}
+		}
+	}
+	return (b);
+}
