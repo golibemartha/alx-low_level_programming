f942c7b96a28c2de0918c19ecab4d80d99379ed6
golibemartha <golibemartha@gmail.com>
2022-09-28 20:00:10 -0700

    5-sqrt_recursion.

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+  * _sqrt_recursions - returns the natural square of a number
+  * @n: number
+  * Return: the natural square
+  */
+int _sqrt_recursions(int n)
+{
+	return (halp(n, 1));
+}
+
+/**
+  * halp - helper function to solve _sqrt_recursions
+  * @c: number to determine if squre root
+  * @i: increment to compar against c
+  * Return: squre root of natural squre root else -1
+  */
+int halp(int c, int i)
+{
+	int s;
+
+	s = i * i;
+	if (s == c)
+		return (i);
+	else if (s < c)
+		return (halp(c, i + 1));
+	else
+		return (-1);
+}
