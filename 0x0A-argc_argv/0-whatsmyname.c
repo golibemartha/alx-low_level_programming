e5c9c2eec7ff901222d4fa6696722202a2c8b8ad
golibemartha <golibemartha@gmail.com>
2022-09-30 06:52:43 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
 /**
   * main - prints its name followed by a new line
   * @argc: argument count
-  * @argv: argument vector 
+  * @argv: argument vector
   * Return: 0
   */
 int main(int argc, char *argv[])
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 	(void) argc;
 
 	int i = 0;
-	
+
 	printf("%s\n", argv[i]);
 
 	return (i);
