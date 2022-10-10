74dfa983f34fffc8249259df19614efa4c3f7ec6
golibemartha <golibemartha@gmail.com>
2022-10-10 03:05:56 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * malloc_checked - allocates memory using malloc
+  * @b: memory size
+  * Return: void pointer
+  */
+void *malloc_checked(unsigned int b)
+{
+	void *a;
+
+	a = malloc(b);
+	if (a == NULL)
+		exit(98);
+	return (a);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,3 @@
+#include <stdlib.h>
+int _putchar(char c);
+void *malloc_cheacked(unsigned int b);
