3fd27981e1ba5716042d292f915658b13b51d8bb
golibemartha <golibemartha@gmail.com>
2022-10-20 21:34:13 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,35 +1,46 @@
 #include "lists.h"
-#include <string.h>
-#include <stdio.h>
 
 /**
- * add_node - add a new node at the beginning of `list_t` list
- * @head: double pointer to head
- * @str: string to be saved in new node, must be duplicated
- * Return: Address of new element or NULL if failed
- */
+ * add_node - adds a node at the beginning of the linked list
+ *
+ * @head: pointer to the first node of the list
+ * @str: string to add to new node in the list
+ *
+ * Return: NULL if it fails / starting addr of the list
+*/
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	int c;
+	size_t str_len = 0;
 
-	for (c = 0; dup_str[c] != '\0'; c++)
-		;
+	/*set strig length to 0 if its NULL*/
+	if (str == NULL)
+		str_len = 0;
+
+	/*count length of string*/
+	while (str[str_len] != '\0')
+		str_len++;
+
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-	{
-		free(new_node);
 		return (NULL);
-	}
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len = c;
-	new_node->next = *head;
 
+	/**
+	 * initialize the next addr of new_node
+	 * to NULL if head is NULL, if it's not
+	 * move the first node to the addr of new_node
+	 * ->next hence adding new_node at the beginning
+	 *  of the list
+	*/
+	if (*head == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
+
+	new_node->str = strdup(str);
+	new_node->len = str_len;
 	*head = new_node;
+
 	return (*head);
 }
